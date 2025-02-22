2e8fe0d76098cdbf77f79be8c23439875fb62a6a
Sampada Kulkarni <kulkarni.samp@northeastern.edu>
2025-02-22 15:18:36 -0800

    Changing layout - redesigning from scratch

--- app/src/main/java/org/kulkarni_sampada/travelpal/MainActivity.java
+++ app/src/main/java/org/kulkarni_sampada/travelpal/MainActivity.java
@@ -1,8 +1,6 @@
 package org.kulkarni_sampada.travelpal;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -12,14 +10,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        // Find the buttons in the layout
-        Button loginButton = findViewById(R.id.loginButton);
-
-        loginButton.setOnClickListener(v -> {
-            // Start the login activity
-            startActivity(new Intent(MainActivity.this, LoginActivity.class));
-            finish();
-        });
     }
 }
